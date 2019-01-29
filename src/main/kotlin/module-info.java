a237694bdf21832fb10d324b7ce18279290fa90d
Maurice Müller <1540618+Maurice-Mueller@users.noreply.github.com>
2019-01-29 14:32:41 +0100

    excluded module-info from javadoc

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,5 +1,5 @@
 module com.esentri.wrabbitmq {
-   requires kotlin.stdlib;
+   requires transitive kotlin.stdlib;
    requires kotlin.stdlib.jdk8;
    //requires org.slf4j;
    //requires com.rabbitmq;
